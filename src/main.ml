7e04df9c4c9bd1620690a667a9307237543444fb
Anders Fugmann <af@issuu.com>
2015-05-11 09:49:32 +0200

    Better seperaration between revisor and main. Revisor now only acts on its own type, and does not hold global state

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,12 @@
 open Batteries
 open Log
 
+type name = string
+type t = {
+  procs: (name, Revisor.t) Hashtbl.t;
+  pids: (int, name) Hashtbl.t;
+}
+
 let reap_children t =
   match Unix.waitpid [Unix.WNOHANG] (-1) with
   | 0, _ ->
@@ -8,7 +14,7 @@ let reap_children t =
   | pid, Unix.WSTOPPED sig_no ->
     (* log "Child stopped: %d(%d)" pid sig_no; *)
     (* Detatch if we dont want to listen anymore *)
-    (match Hashtbl.mem t.Revisor.pids pid with
+    (match Hashtbl.mem t.pids pid with
     | true -> Extern.ptrace_cont pid sig_no
     | false ->
       log `Info "Detach pid: %d. Not ours" pid;
@@ -26,6 +32,15 @@ let reap_children t =
     log `Debug "Exception in signal handler: %s" (Printexc.to_string e);
     None
 
+let update_proc t proc =
+  Hashtbl.replace t.procs proc.Revisor.spec.Process.name proc;
+  match Revisor.is_running proc with
+  | false -> ()
+  | true ->
+    Revisor.pids proc
+    |> List.map fst
+    |> List.iter (fun p -> Hashtbl.replace t.pids p proc.Revisor.spec.Process.name)
+
 (* Setup an alarm, and match on that. I dont like control flow using signals *)
 let rec event_loop signal_fd t =
   begin
@@ -34,13 +49,17 @@ let rec event_loop signal_fd t =
       ExtUnixAll.signalfd_read signal_fd |> ignore;
       reap_children t
       |> Option.may
-      @@ fun p ->
-      let name = Hashtbl.find t.Revisor.pids p in
-      Revisor.process_term t name p;
-      Revisor.check t name
+        (fun p ->
+           let name = Hashtbl.find t.pids p in
+           Hashtbl.remove t.pids p;
+           Hashtbl.find t.procs name
+           |> (fun proc -> Revisor.process_term proc p)
+           |> Revisor.check
+           |> update_proc t
+        )
     | _ ->
       (* Could be starved, if a process dies every second *)
-      Revisor.check_all t
+      Hashtbl.map_inplace (fun _ proc -> Revisor.check proc) t.procs
   end;
   event_loop signal_fd t
 
@@ -49,7 +68,7 @@ let reload t =
   specs
   |> List.iter (fun spec ->
       spec |> Process.to_yojson |> Yojson.Safe.pretty_to_string |> print_endline;
-      Revisor.reload_spec t spec
+      Hashtbl.modify_opt spec.Process.name (fun proc -> Some (Revisor.update_spec spec proc)) t.procs
     );
 
   let current =
@@ -59,10 +78,10 @@ let reload t =
   in
   log `Info "Number of processes: %d" (Set.cardinal current);
 
-  Hashtbl.keys t.Revisor.procs
+  Hashtbl.keys t.procs
   |> Set.of_enum
   |> (flip Set.diff) current
-  |> Set.iter (fun name -> Revisor.set_target t name Revisor.Disabled)
+  |> Set.iter (fun name -> Hashtbl.modify name (fun proc -> Revisor.set_target proc Revisor.Disabled) t.procs)
 
 let attach (_name, pid) =
   (* No need to detatch, as we do that when reaping child processes *)
@@ -70,35 +89,44 @@ let attach (_name, pid) =
   && Extern.ptrace_seize (fst pid) == 0
   && Process.is_running pid
 
+let load_state () =
+  Sys.readdir Config.run_dir
+  |> Array.enum
+  |> Enum.filter (fun f -> Filename.check_suffix f Config.run_suffix)
+  |> Enum.map (fun l -> Config.run_dir ^ "/" ^ l)
+  |> Enum.filter_map Revisor.load
+  |> Enum.map (fun t -> t.Revisor.spec.Process.name, t)
+  |> Hashtbl.of_enum
+
 
 let main () =
   Unix.(sigprocmask SIG_BLOCK [ Sys.sigchld ]) |> ignore;
   let signal_fd = ExtUnixAll.signalfd ~sigs:[ Sys.sigchld ] ~flags:[] () in
 
-  let procs = Revisor.init () in
+  let procs = load_state () in
   (* Attach all pids *)
 
   let (pids, terminated) =
     procs
     |> Hashtbl.enum
-    |> Enum.map (fun (name, e) -> name, Revisor.pids_of_state (snd e.Revisor.state))
+    |> Enum.map (fun (name, e) -> name, Revisor.pids e)
     |> Enum.concat_map (fun (name, pids) -> List.map (fun p -> name, p) pids |> List.enum)
     |> Enum.switch attach
   in
   log `Info "Reattached to %d pids" (Enum.count pids);
   log `Info "Pids stopped while down: %d" (Enum.count terminated);
+
   (* pids are all the pids that are attached and running *)
   (* missing are all pids that are not ours or are not running *)
   let pids =
     pids |> Enum.map (fun (name, (pid, _)) -> pid, name) |> Hashtbl.of_enum
   in
 
-  let t = { Revisor.procs; pids } in
-
   (* Send process term signal to all processes. *)
   terminated
-  |> Enum.iter (fun (name, (s_pid, _)) -> Revisor.process_term t name s_pid);
+  |> Enum.iter (fun (name, (s_pid, _)) -> Hashtbl.modify name (fun p -> Revisor.process_term p s_pid) procs);
 
+  let t = { procs; pids } in
   reload t;
 
   event_loop signal_fd t
--- src/revisor.ml
+++ src/revisor.ml
@@ -17,27 +17,22 @@ type state = Running of pid * pid
            | Stopping of pid option * pid option * ts
            | Stopped [@@deriving yojson]
 
-type elt = { spec: Process.t;
-             state: (ts * state);
-             target: target_state;
-           } [@@deriving yojson]
-
-type t = {
-  procs: (name, elt) Hashtbl.t;
-  pids: (pid_t, name) Hashtbl.t;
-}
+type t = { spec: Process.t;
+           state: (ts * state);
+           target: target_state;
+         } [@@deriving yojson]
 
 let now () = Unix.gettimeofday () *. 1000.0 |> truncate
 
-let save name elt =
-  let file = (Config.run_dir ^ "/" ^ name ^ Config.run_suffix) in
-  elt_to_yojson elt
+let save t =
+  let file = (Config.run_dir ^ "/" ^ t.spec.Process.name ^ Config.run_suffix) in
+  to_yojson t
   |> Yojson.Safe.to_file file
 
 let load filename =
   filename
   |> Yojson.Safe.from_file
-  |> elt_of_yojson
+  |> of_yojson
   |> (function `Ok elt -> Some elt
             | `Error s ->
               log `Warning "Error loading file: %s" s;
@@ -55,60 +50,42 @@ let state_eq = function
   | Stopped    -> (function Stopped    -> true | _ -> false)
 
 
-let update_state t name new_state =
-  let update new_state pd =
-    let (old_ts, old_state) = pd.state in
-    let state = match state_eq new_state old_state with
-      | true -> (old_ts, new_state)
-      | false ->
-        let now = now () in
-        Log.log `Debug "%s: %s -> %s after %d msec"
-          name
-          (name_of_state old_state)
-          (name_of_state new_state)
-          (now - old_ts);
-        (now, new_state)
-    in
-    { pd with state; }
+let update_state t new_state =
+  let (old_ts, old_state) = t.state in
+  let state = match state_eq new_state old_state with
+    | true -> (old_ts, new_state)
+    | false ->
+      let now = now () in
+      Log.log `Debug "%s: %s -> %s after %d msec"
+        t.spec.Process.name
+        (name_of_state old_state)
+        (name_of_state new_state)
+        (now - old_ts);
+      (now, new_state)
   in
-  Hashtbl.modify name (update new_state) t.procs;
-  save name (Hashtbl.find t.procs name) (* We should just pass the elt *)
-
-let state_of_name t name =
-  let s = Hashtbl.find t.procs name in
-  snd s.state
-
-let spec_of_name t name =
-  let s = Hashtbl.find t.procs name in
-  s.spec
+  { t with state; } |> tap save
 
 let kill signal pid =
   match Process.is_running pid with
   | true -> Unix.kill (fst pid) signal
   | false -> ()
 
-let update_pid t name (pid, _) =
-  Hashtbl.add t.pids pid name
-
-let remove_pid t (pid, _) =
-  Hashtbl.remove t.pids pid
 
 (* Create a function for chaning state - To log and save state changes *)
-let process_start t name =
-  match state_of_name t name with
-  | Running _ -> ()
-  | Stopping _ -> ()
-  | Stopped  ->
-    (* Start the process *)
-    let pd = spec_of_name t name in
-    let (pid, r_pid) = Process.start pd in
-    update_pid t name pid;
-    update_pid t name r_pid;
-    Running (pid, r_pid) |> update_state t name
+let process_start t =
+  let new_state = match snd t.state with
+    | Running _ as s -> s
+    | Stopping _ as s -> s
+    | Stopped  ->
+      (* Start the process *)
+      let (pid, r_pid) = Process.start t.spec in
+      Running (pid, r_pid)
+  in
+  update_state t new_state
 
-let process_stop t name =
+let process_stop t =
   let now = now () in
-  let new_state = match state_of_name t name with
+  let new_state = match snd t.state with
     | Running (pid, r_pid) ->
       kill Sys.sigterm pid;
       Stopping (Some pid, Some r_pid, now)
@@ -121,94 +98,77 @@ let process_stop t name =
     | Stopping _ as s -> s
     | Stopped as s -> s
   in
-  update_state t name new_state
+  update_state t new_state
 
-let process_term t name s_pid =
-  match state_of_name t name with
-  | Running (pid, r_pid) when s_pid = fst pid ->
-    (* log "Received signal from %s (%d)" name pid; *)
-    remove_pid t pid;
-    Stopping (None, Some r_pid, now ()) |> update_state t name
+let process_term t s_pid =
+  let new_state = match snd t.state with
+    | Running (pid, r_pid) when s_pid = fst pid ->
+      (* log "Received signal from %s (%d)" name pid; *)
+      Stopping (None, Some r_pid, now ())
 
-  | Running (pid, r_pid) when s_pid = fst r_pid ->
-    (* log "Failure: one of the redirect processes for %s died" name; *)
-    kill Sys.sigterm pid;
-    Stopping (Some pid, None, now ()) |> update_state t name
+    | Running (pid, r_pid) when s_pid = fst r_pid ->
+      (* log "Failure: one of the redirect processes for %s died" name; *)
+      kill Sys.sigterm pid;
+      Stopping (Some pid, None, now ())
 
-  | Stopping (Some pid, None, _ts) when s_pid = fst pid ->
-    Stopped |> update_state t name
-  | Stopping (None, Some r_pid, _ts) when s_pid = fst r_pid ->
-    Stopped |> update_state t name
-  | Stopping (Some pid, Some r_pid, _ts) when s_pid = fst pid ->
-    (* The main process finally stopped. Send the logger a term *)
-    kill Sys.sigterm r_pid;
-    Stopping (None, Some r_pid, now()) |> update_state t name
+    | Stopping (Some pid, None, _ts) when s_pid = fst pid ->
+      Stopped
+    | Stopping (None, Some r_pid, _ts) when s_pid = fst r_pid ->
+      Stopped
+    | Stopping (Some pid, Some r_pid, _ts) when s_pid = fst pid ->
+      (* The main process finally stopped. Send the logger a term *)
+      kill Sys.sigterm r_pid;
+      Stopping (None, Some r_pid, now())
 
-  | Stopping (Some pid, Some r_pid, ts) when s_pid = fst r_pid ->
-    (* This is strange. We send a kill signal to main process, and now the
-       redicrect process terminated? *)
-    Stopping (Some pid, None, ts) |> update_state t name
+    | Stopping (Some pid, Some r_pid, ts) when s_pid = fst r_pid ->
+      (* This is strange. We sent a kill signal to main process, and now the
+         redicrect process terminated? *)
+      Stopping (Some pid, None, ts)
 
-  | state ->
-    log `Fatal "Illegal state for term: %s" (state_to_yojson state |> Yojson.Safe.pretty_to_string);
-
-    failwith (Printf.sprintf "pid %d is not a member of %s" s_pid name)
+    | state ->
+      log `Fatal "Illegal state for term: %s" (state_to_yojson state |> Yojson.Safe.pretty_to_string);
+      failwith (Printf.sprintf "pid %d is not a member of %s" s_pid t.spec.Process.name)
+  in
+  update_state t new_state
 
 let string_of_target = function
   | Enabled -> "enabled"
   | Disabled -> "disabled"
 
-let rec check t name =
-  match Hashtbl.find t.procs name with
-  | { state = _, Stopping (_, _, ts);  _ } when ts + 5 < (now ()) ->
-    process_stop t name;
-    check t name
-  | { state = _, Stopping (None, None, _); _ } ->
-    process_stop t name;
-    check t name
-  | { state = _, Running _; target = Disabled; _ } ->
-    process_stop t name;
-    check t name
-  | { state = _, Stopped; target = Enabled; _} ->
-    process_start t name;
-    check t name
-  | _ -> ()
+let rec check = function
+  | { state = _, Stopping (_, _, ts); _ } as t when ts + 5 < (now ()) ->
+    process_stop t |> check
+  | { state = _, Stopping (None, None, _); _ } as t ->
+    process_stop t |> check
+  | { state = _, Running _; target = Disabled; _ } as t ->
+    process_stop t |> check
+  | { state = _, Stopped; target = Enabled; _} as t ->
+    process_start t |> check
+  | t -> t
 
-let check_all t =
-  t.procs
-  |> Hashtbl.keys
-  |> Enum.iter (check t)
-
-let pids_of_state = function
+let pids t =
+  match snd t.state with
   | Running(p1, p2) -> [p1; p2]
   | Stopping(p1, p2, _) -> List.filter_map identity [p1; p2]
   | Stopped -> []
 
+let is_running t =
+  match snd t.state with
+  | Running _ -> true
+  | Stopping _
+  | Stopped -> false
 
-let set_target t name target =
-  log `Info "Setting target of process %s to %s" name (string_of_target target);
-  Hashtbl.modify name (fun p -> { p with target }) t.procs;
-  check t name
+let set_target t target =
+  log `Info "Setting target of process %s to %s" t.spec.Process.name (string_of_target target);
+  { t with target } |> check
 
-let init () =
-  Sys.readdir Config.run_dir
-  |> Array.enum
-  |> Enum.filter (fun f -> Filename.check_suffix f Config.run_suffix)
-  |> Enum.map (fun l -> Config.run_dir ^ "/" ^ l)
-  |> Enum.filter_map load
-  |> Enum.map (fun elt -> elt.spec.Process.name, elt)
-  |> Hashtbl.of_enum
-
-let reload_spec t spec =
-  let open Process in
-  match Hashtbl.find_option t.procs spec.name with
+let update_spec spec = function
   | None ->
-      Hashtbl.add t.procs spec.name { spec; state = (now(), Stopped); target = Enabled; }
-  | Some { spec = spec'; _ } when spec = spec' -> ()
+      { spec; state = (now(), Stopped); target = Enabled; }
+  | Some ({ spec = spec'; _ } as s) when spec = spec' -> s
   | Some { state; _ } -> (* Apparently the state has been changed *)
-    let target = match spec.auto_start with
+    let target = match spec.Process.auto_start with
       | true -> Enabled
       | false -> Disabled
     in
-    Hashtbl.replace t.procs spec.name { spec ; target; state };
-    process_stop t spec.name
+    { spec ; target; state } |> process_stop |> check
